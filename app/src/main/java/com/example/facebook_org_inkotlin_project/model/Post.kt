299a8b1f7a63ae80b1313758b5e237015a2d79c5
IlhombekUbaydullayev <ubaydullaevilhombek682@gmail.com>
2022-02-19 04:38:29 +0500

    facebook

---
+++ app/src/main/java/com/example/facebook_org_inkotlin_project/model/Post.kt
@@ -0,0 +1,4 @@
+package com.example.facebook_org_inkotlin_project.model
+
+class Post(var profile:Int,var fullName:String,var photo : Int,var photo2 : Int = 0,var photo3 : Int = 0,var photo4 : Int = 0,var photo5 : Int = 0,var isAvailable : Int = 0) {
+}
---
+++ app/src/main/java/com/example/facebook_org_inkotlin_project/model/Story.kt
@@ -0,0 +1,4 @@
+package com.example.facebook_org_inkotlin_project.model
+
+class Story(var profile : Int, var fullname : String,var isAvailable : Boolean = false) {
+}
